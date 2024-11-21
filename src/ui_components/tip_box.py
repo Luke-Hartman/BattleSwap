0765677930dc47bb89421b393452460db1a262b3
Luke Hartman <lukehartman123@gmail.com>
2024-11-20 20:06:17 -0500

    Fix text box so it doesn't freak out.

--- src/ui_components/tip_box.py
+++ src/ui_components/tip_box.py
@@ -4,14 +4,17 @@ from battles import Battle
 
 class TipBox(pygame_gui.elements.UITextBox):
     def __init__(self, manager: pygame_gui.UIManager, battle: Battle) -> None:
+        width = max(len(line) for line in battle.tip) * 8
+        height = len(battle.tip) * 20
         super().__init__(
             html_text='<br>'.join(battle.tip),
             relative_rect=pygame.Rect(
                 (pygame.display.Info().current_w//2, 10),
-                (-1, -1)
+                (width, height)
             ),
             manager=manager,
-            wrap_to_height=True
+            wrap_to_height=True,
+            object_id="#tip_box"
         )
         # Center the tip box horizontally
         window_width = pygame.display.Info().current_w
