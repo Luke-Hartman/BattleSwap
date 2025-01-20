e27aaf3c51a0a95564485371ec0792550eb7161d
luke <luke.hartman@tmlt.io>
2025-01-20 13:32:57 -0500

    Disable nudging.

--- src/processors/nudge_processor.py
+++ src/processors/nudge_processor.py
@@ -10,17 +10,18 @@ from game_constants import gc
 class NudgeProcessor:
 
     def process(self, dt: float):
-        for entity, (pos, unit_state) in esper.get_components(Position, UnitState):
-            if unit_state.state != State.PURSUING or esper.has_component(entity, NoNudge):
-                continue
-            for other_entity, (other_pos, other_unit_state) in esper.get_components(Position, UnitState):
-                if other_entity == entity or other_unit_state.state == State.DEAD or esper.has_component(other_entity, NoNudge):
-                    continue
-                distance = pos.distance(other_pos, 1)
-                if distance < gc.NUDGE_DISTANCE:
-                    if distance < 0.001:
-                        continue
-                    nudge_distance = min(gc.NUDGE_DISTANCE - distance, 1)
-                    pos.x += nudge_distance * (pos.x - other_pos.x) / distance
-                    pos.y += nudge_distance * (pos.y - other_pos.y) / distance - 0.3
-
+        pass
+        # DISABLED FOR NOW
+        # for entity, (pos, unit_state) in esper.get_components(Position, UnitState):
+        #     if unit_state.state != State.PURSUING or esper.has_component(entity, NoNudge):
+        #         continue
+        #     for other_entity, (other_pos, other_unit_state) in esper.get_components(Position, UnitState):
+        #         if other_entity == entity or other_unit_state.state == State.DEAD or esper.has_component(other_entity, NoNudge):
+        #             continue
+        #         distance = pos.distance(other_pos, 1)
+        #         if distance < gc.NUDGE_DISTANCE:
+        #             if distance < 0.001:
+        #                 continue
+        #             nudge_distance = min(gc.NUDGE_DISTANCE - distance, 1)
+        #             pos.x += nudge_distance * (pos.x - other_pos.x) / distance
+        #             pos.y += nudge_distance * (pos.y - other_pos.y) / distance
