58157e42f6484c0a803f82c2f069818a6b1f57e1
luke <luke.hartman@tmlt.io>
2025-03-20 09:39:16 -0400

    Performance improvements to targetting.

--- src/processors/targetting_processor.py
+++ src/processors/targetting_processor.py
@@ -6,26 +6,24 @@ from components.ability import Abilities
 from components.destination import Destination
 from components.instant_ability import InstantAbilities
 from components.unit_state import State, UnitState
-from target_strategy import TargetStrategy
 
 
 class TargettingProcessor(esper.Processor):
     """Processor responsible for targetting."""
 
     def process(self, dt: float):
+        target_strategies = set()
         for ent, (unit_state, destination) in esper.get_components(UnitState, Destination):
-            self._update_target(ent, unit_state, destination.target_strategy)
+            target_strategies.add((ent, unit_state.state, destination.target_strategy))
         for ent, (unit_state, instant_abilities) in esper.get_components(UnitState, InstantAbilities):
             for ability in instant_abilities.abilities:
-                self._update_target(ent, unit_state, ability.target_strategy)
+                target_strategies.add((ent, unit_state.state, ability.target_strategy))
         for ent, (unit_state, abilities) in esper.get_components(UnitState, Abilities):
             for ability in abilities.abilities:
-                self._update_target(ent, unit_state, ability.target_strategy)
-
-    def _update_target(self, ent: int, unit_state: UnitState, target_strategy: TargetStrategy):
-        """Update the target for the given entity."""
-        # Consider new targets
-        if unit_state.state == State.IDLE or unit_state.state == State.PURSUING:
-            target_strategy.find_target()
-        elif unit_state.state == State.DEAD:
-            target_strategy.target = None
+                target_strategies.add((ent, unit_state.state, ability.target_strategy))
+        for ent, state, target_strategy in target_strategies:
+            # Consider new targets
+            if state == State.IDLE or state == State.PURSUING:
+                target_strategy.find_target()
+            elif state == State.DEAD:
+                target_strategy.target = None
