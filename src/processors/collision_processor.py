Luke-Hartman/BattleSwap

commit 64b92c3161a2f1915fbf1b195e86357993657532

Tweak wizard/catapult collision and improve effect layering.

src/processors/collision_processor.py
@@ -7,19 +7,16 @@ detecting collisions between projectiles and units of opposing teams.
 from typing import Tuple
 import esper
 import pygame
-import numpy as np
-from scipy.spatial import KDTree
-from shapely import Polygon
 from components.position import Position
-from game_constants import gc
-from components.aoe import AoE
+from components.aoe import VisualAoE, CircleAoE
 from components.projectile import Projectile
 from components.sprite_sheet import SpriteSheet
 from components.team import Team, TeamType
 from components.unit_state import UnitState, State
 from components.hitbox import Hitbox
-from events import AOE_HIT, AoEHitEvent, ProjectileHitEvent, PROJECTILE_HIT, emit_event
+from events import CIRCLE_AOE_HIT, VISUAL_AOE_HIT, CircleAoEHitEvent, ProjectileHitEvent, PROJECTILE_HIT, VisualAoEHitEvent, emit_event
 from hex_grid import get_hex_bounds
+from unit_condition import MaximumDistanceFromEntity
 
 class CollisionProcessor(esper.Processor):
     """Processor responsible for detecting collisions between projectiles and units of opposing teams."""
@@ -32,10 +29,10 @@ class CollisionProcessor(esper.Processor):
         team2_projectiles = pygame.sprite.Group()
         team1_units = pygame.sprite.Group()
         team2_units = pygame.sprite.Group()
-        aoe_sprites = pygame.sprite.Group()
+        visual_aoe_sprites = pygame.sprite.Group()
+        circle_aoe_ents = []
         sprite_to_ent = {}
 
-        # Separate entities by team and type (projectiles, AoEs, or units)
         for ent, (sprite, team) in esper.get_components(SpriteSheet, Team):
             sprite_to_ent[sprite] = ent
             if esper.has_component(ent, Projectile):
@@ -43,8 +40,8 @@ class CollisionProcessor(esper.Processor):
                     team1_projectiles.add(sprite)
                 else:
                     team2_projectiles.add(sprite)
-            elif esper.has_component(ent, AoE):
-                aoe_sprites.add(sprite)
+            elif esper.has_component(ent, VisualAoE):
+                visual_aoe_sprites.add(sprite)
             elif esper.has_component(ent, UnitState):
                 unit_state = esper.component_for_entity(ent, UnitState)
                 if unit_state.state != State.DEAD:
@@ -53,8 +50,11 @@ class CollisionProcessor(esper.Processor):
                     else:
                         team2_units.add(sprite)
         
+        for ent, (_) in esper.get_components(CircleAoE):
+            circle_aoe_ents.append(ent)
+        
         # For all sprites, update their collision masks
-        for sprite in [*team1_projectiles, *team2_projectiles, *team1_units, *team2_units, *aoe_sprites]:
+        for sprite in [*team1_projectiles, *team2_projectiles, *team1_units, *team2_units]:
             sprite.mask = pygame.mask.from_surface(sprite.image, threshold=10)
 
         # Handle collisions between team1 projectiles and team2 units
@@ -63,14 +63,22 @@ class CollisionProcessor(esper.Processor):
         # Handle collisions between team2 projectiles and team1 units
         self.process_unit_projectile_collisions(team2_projectiles, team1_units, sprite_to_ent)
 
-        # Handle collisions between AoEs and units
-        self.process_aoe_unit_collisions(aoe_sprites, team1_units, sprite_to_ent)
-        self.process_aoe_unit_collisions(aoe_sprites, team2_units, sprite_to_ent)
+        # Handle collisions between Visual AOEs and units
+        self.process_visual_aoe_unit_collisions(visual_aoe_sprites, team1_units, sprite_to_ent)
+        self.process_visual_aoe_unit_collisions(visual_aoe_sprites, team2_units, sprite_to_ent)
+        
+        # Handle collisions between Circle AOEs and units
+        self.process_circle_aoe_unit_collisions(circle_aoe_ents, team1_units, sprite_to_ent)
+        self.process_circle_aoe_unit_collisions(circle_aoe_ents, team2_units, sprite_to_ent)
 
         # Remove off-battlefield projectiles
         for projectile in [*team1_projectiles, *team2_projectiles]:
             if not self.battlefield_rect.colliderect(projectile.rect):
                 esper.delete_entity(sprite_to_ent[projectile])
+
+        # Remove all circle aoes
+        for aoe_ent in circle_aoe_ents:
+            esper.delete_entity(aoe_ent)
 
     def check_sprite_group_collisions(self, group1: pygame.sprite.Group, group2: pygame.sprite.Group) -> list[tuple[pygame.sprite.Sprite, pygame.sprite.Sprite]]:
         """Check collisions between two sprite groups."""
@@ -152,17 +160,44 @@ class CollisionProcessor(esper.Processor):
             collided_projectiles.add(p_sprite)
             esper.delete_entity(p_ent)
 
-    def process_aoe_unit_collisions(
+    def process_visual_aoe_unit_collisions(
         self,
         aoe_sprites: pygame.sprite.Group,
         u_sprites: pygame.sprite.Group,
         sprite_to_ent: dict
     ):
-        """Handle collisions between AoEs and units."""
+        """Handle collisions between visual AOEs and units."""
         collisions = self.check_sprite_group_collisions(aoe_sprites, u_sprites)
+
         for aoe_sprite, u_sprite in collisions:
-            if not self.check_hitbox_collision(aoe_sprite, u_sprite, sprite_to_ent):
-                continue
             aoe_ent = sprite_to_ent[aoe_sprite]
             u_ent = sprite_to_ent[u_sprite]
-            emit_event(AOE_HIT, event=AoEHitEvent(entity=aoe_ent, target=u_ent))
+            aoe = esper.component_for_entity(aoe_ent, VisualAoE)
+
+            if u_ent in aoe.hit_entities:
+                continue
+            if not aoe.unit_condition.check(u_ent):
+                continue
+            aoe.hit_entities.append(u_ent)
+            emit_event(VISUAL_AOE_HIT, event=VisualAoEHitEvent(entity=aoe_ent, target=u_ent))
+
+    def process_circle_aoe_unit_collisions(
+        self,
+        circle_aoe_ents: list[int],
+        u_sprites: pygame.sprite.Group,
+        sprite_to_ent: dict
+    ):
+        """Handle collisions between CircleAoEs and units."""
+        # Process all CircleAoEs
+        for aoe_ent in circle_aoe_ents:
+            aoe = esper.component_for_entity(aoe_ent, CircleAoE)
+            range_condition = MaximumDistanceFromEntity(aoe_ent, distance=aoe.radius, y_bias=None, use_hitbox=True)
+            # Check all units
+            for u_sprite in u_sprites:
+                u_ent = sprite_to_ent[u_sprite]
+                # Check unit condition
+                if not aoe.unit_condition.check(u_ent):
+                    continue
+                if not range_condition.check(u_ent):
+                    continue
+                emit_event(CIRCLE_AOE_HIT, event=CircleAoEHitEvent(entity=aoe_ent, target=u_ent))

src/components/aoe.py
@@ -1,5 +1,12 @@
 """Component for area of effect abilities.
 
+This module contains components for different types of area of effect (AoE) abilities.
+There are two main types:
+1. VisualAoE - an entity that affects other entities which collide with it using pixel-perfect collision 
+   detection and has a visual representation.
+2. CircleAoE - an entity that affects other entities within a specified radius without a visual representation
+   that hits instantly.
+
 The primary difference between an AoE and an Aura, is than an AoE is an entity that
 affects other entities which collide with it, while an Aura is a continuous effect
 that affects entities in a radius of another entity.
@@ -14,8 +21,8 @@ if TYPE_CHECKING:
     from unit_condition import UnitCondition
 
 @dataclass
-class AoE:
-    """The AoE component."""
+class VisualAoE:
+    """The VisualAoE component for pixel-based collision with visual representation."""
 
     effects: List["Effect"]
     owner: Optional[int]
@@ -24,3 +31,15 @@ class AoE:
     """Condition that determines which units are affected by the AoE."""
     hit_entities: List[int] = field(default_factory=list)
     """Entities that have already been hit by the AoE."""
+
+@dataclass
+class CircleAoE:
+    """The CircleAoE component for radius-based collision without visual representation."""
+
+    effects: List["Effect"]
+    owner: Optional[int]
+    """Owner is used to apply buffs/debuffs to AoE effects."""
+    unit_condition: "UnitCondition"
+    """Condition that determines which units are affected by the AoE."""
+    radius: float
+    """The radius to use for collision detection."""

src/effects.py
@@ -14,7 +14,7 @@ from components.ammo import Ammo
 from components.angle import Angle
 from components.angular_velocity import AngularVelocity
 from components.animation import AnimationState, AnimationType
-from components.aoe import AoE
+from components.aoe import CircleAoE, VisualAoE
 from components.armor import Armor
 from components.attached import Attached
 from components.aura import Aura
@@ -141,8 +141,8 @@ class Heals(Effect):
 
 
 @dataclass
-class CreatesAoE(Effect):
-    """Effect creates an AoE at the location of the parent."""
+class CreatesVisualAoE(Effect):
+    """Effect creates a VisualAoE at the location of the parent."""
 
     effects: List[Effect]
     """The effect of the AoE."""
@@ -186,16 +186,17 @@ class CreatesAoE(Effect):
         orientation = esper.try_component(recipient, Orientation)
         esper.add_component(entity, Position(x=position.x, y=position.y))
         esper.add_component(entity, Team(type=team.type))
-        esper.add_component(entity, AoE(
+        esper.add_component(entity, VisualAoE(
             effects=self.effects,
             owner=owner,
-            unit_condition=self.unit_condition
+            unit_condition=self.unit_condition,
         ))
         esper.add_component(entity, create_visual_spritesheet(
             visual=self.visual,
             scale=self.scale,
             duration=self.duration + 1/30,
-            frames=self.visual_frames
+            frames=self.visual_frames,
+            layer=2,
         ))
         esper.add_component(entity, AnimationState(type=AnimationType.IDLE))
         esper.add_component(entity, Expiration(time_left=self.duration))
@@ -203,6 +204,47 @@ class CreatesAoE(Effect):
             esper.add_component(entity, Orientation(facing=orientation.facing))
         if self.on_create:
             self.on_create(entity)
+
+
+@dataclass
+class CreatesCircleAoE(Effect):
+    """Effect creates a CircleAoE at the specified location."""
+
+    effects: List[Effect]
+    """The effect of the AoE."""
+
+    radius: float
+    """The radius of the CircleAoE."""
+
+    unit_condition: "UnitCondition"
+    """Condition that determines which units are affected by the AoE."""
+
+    location: Recipient
+    """The location of the AoE."""
+
+    def apply(self, owner: Optional[int], parent: Optional[int], target: Optional[int]) -> None:
+        entity = esper.create_entity()
+        if self.location == Recipient.OWNER:
+            assert owner is not None
+            recipient = owner
+        elif self.location == Recipient.PARENT:
+            assert parent is not None
+            recipient = parent
+        elif self.location == Recipient.TARGET:
+            assert target is not None
+            recipient = target
+        else:
+            raise ValueError(f"Invalid location: {self.location}")
+        position = esper.component_for_entity(recipient, Position)
+        team = esper.component_for_entity(recipient, Team)
+        esper.add_component(entity, Position(x=position.x, y=position.y))
+        esper.add_component(entity, Team(type=team.type))
+        esper.add_component(entity, CircleAoE(
+            effects=self.effects,
+            owner=owner,
+            unit_condition=self.unit_condition,
+            radius=self.radius
+        ))
 
 
 @dataclass
@@ -266,7 +308,7 @@ class CreatesProjectile(Effect):
         esper.add_component(entity, Orientation(facing=parent_orientation.facing))
         esper.add_component(entity, Team(type=parent_team.type))
         esper.add_component(entity, Projectile(effects=self.effects, owner=owner, unit_condition=self.unit_condition))
-        esper.add_component(entity, create_visual_spritesheet(self.visual))
+        esper.add_component(entity, create_visual_spritesheet(self.visual, layer=1))
         esper.add_component(entity, AnimationState(type=AnimationType.IDLE))
         if self.on_create:
             self.on_create(entity)
@@ -426,8 +468,8 @@ class CreatesAttachedVisual(Effect):
 
 
 @dataclass
-class CreatePermanentVisual(Effect):
-    """Effect creates a visual effect at the location of the recipient that is permanent."""
+class CreatesVisual(Effect):
+    """Effect creates a visual effect at the location of the recipient with an optional duration."""
 
     recipient: Recipient
     """The recipient of the effect."""
@@ -436,10 +478,13 @@ class CreatePermanentVisual(Effect):
     """The visual of the effect."""
 
     animation_duration: float
-    """The duration of the animation of the effect."""
+    """The duration of the animation loop."""
 
     scale: float
     """The scale of the effect."""
+
+    duration: Optional[float] = None
+    """The duration of the effect. If None, the effect is permanent."""
 
     offset: Optional[Callable[[int], Tuple[int, int]]] = None
     """The offset of the effect from the target's position."""
@@ -460,17 +505,31 @@ class CreatePermanentVisual(Effect):
         elif self.recipient == Recipient.TARGET:
             assert target is not None
             recipient = target
+        else:
+            raise ValueError(f"Invalid recipient: {self.recipient}")
+            
         recipient_position = esper.component_for_entity(recipient, Position)
         team = esper.component_for_entity(recipient, Team)
         entity = esper.create_entity()
+        
         esper.add_component(entity, Position(x=recipient_position.x, y=recipient_position.y))
         esper.add_component(entity, Team(type=team.type))
-        esper.add_component(entity, create_visual_spritesheet(visual=self.visual, scale=self.scale, layer=self.layer))
+        esper.add_component(entity, create_visual_spritesheet(
+            visual=self.visual,
+            duration=self.duration,
+            scale=self.scale,
+            layer=self.layer
+        ))
+        
         if self.random_starting_frame:
             time_elapsed = random.random() * self.animation_duration
         else:
             time_elapsed = 0
+        
         esper.add_component(entity, AnimationState(type=AnimationType.IDLE, time_elapsed=time_elapsed))
+        if self.duration is not None:
+            esper.add_component(entity, Expiration(time_left=self.duration))
+
 
 
 @dataclass
@@ -773,7 +832,7 @@ class CreatesLobbed(Effect):
             )
         )
         esper.add_component(entity, Position(x=start.x, y=start.y))
-        esper.add_component(entity, create_visual_spritesheet(self.visual))
+        esper.add_component(entity, create_visual_spritesheet(self.visual, layer=1))
         esper.add_component(entity, AnimationState(type=AnimationType.IDLE))
         esper.add_component(entity, Team(type=parent_team.type))
         esper.add_component(entity, Orientation(facing=parent_orientation.facing))

src/entities/units.py
@@ -35,7 +35,12 @@ from components.unit_type import UnitType, UnitTypeComponent
 from components.velocity import Velocity
 from components.health import Health
 from components.orientation import Orientation, FacingDirection
-from effects import AppliesStatusEffect, AttachToTarget, CreatePermanentVisual, CreatesAoE, CreatesAttachedVisual, CreatesLobbed, CreatesProjectile, Damages, Forget, Heals, IncreaseAmmo, Jump, PlaySound, Recipient, SoundEffect, StanceChange, RememberTarget
+from effects import (
+    AppliesStatusEffect, AttachToTarget, CreatesVisual, 
+    CreatesAttachedVisual, CreatesLobbed, CreatesProjectile, Damages, 
+    Forget, Heals, IncreaseAmmo, Jump, PlaySound, Recipient, 
+    SoundEffect, StanceChange, RememberTarget, CreatesVisualAoE, CreatesCircleAoE
+)
 from unit_condition import (
     All, Alive, Always, AmmoEquals, Any, Grounded, HasComponent, HealthBelowPercent, InStance, Infected, IsEntity, IsUnitType, MaximumDistanceFromDestination, MinimumDistanceFromEntity, Never, Not, OnTeam,
     MaximumDistanceFromEntity, RememberedBy, RememberedSatisfies
@@ -397,7 +402,7 @@ def create_core_barbarian(x: int, y: int, team: TeamType) -> int:
                     persistent_conditions=[],
                     effects={
                         6: [
-                            CreatesAoE(
+                            CreatesVisualAoE(
                                 effects=[
                                     Damages(damage=gc.CORE_BARBARIAN_ATTACK_DAMAGE, recipient=Recipient.TARGET),
                                 ],
@@ -802,15 +807,21 @@ def create_core_wizard(x: int, y: int, team: TeamType) -> int:
                             CreatesProjectile(
                                 projectile_speed=gc.CORE_WIZARD_PROJECTILE_SPEED,
                                 effects=[
-                                    CreatesAoE(
+                                    CreatesCircleAoE(
                                         effects=[
                                             Damages(damage=gc.CORE_WIZARD_ATTACK_DAMAGE, recipient=Recipient.TARGET),
                                         ],
-                                        visual=Visual.Explosion,
-                                        duration=gc.CORE_WIZARD_FIREBALL_AOE_DURATION,
-                                        scale=gc.CORE_WIZARD_FIREBALL_AOE_SCALE,
+                                        radius=45.0,
                                         unit_condition=All([Alive(), Grounded()]),
                                         location=Recipient.PARENT,
+                                    ),
+                                    CreatesVisual(
+                                        recipient=Recipient.PARENT,
+                                        visual=Visual.Explosion,
+                                        animation_duration=gc.CORE_WIZARD_FIREBALL_AOE_DURATION,
+                                        scale=gc.CORE_WIZARD_FIREBALL_AOE_SCALE,
+                                        duration=gc.CORE_WIZARD_FIREBALL_AOE_DURATION,
+                                        layer=2,
                                     ),
                                     PlaySound(SoundEffect(filename="fireball_impact.wav", volume=0.50)),
                                 ],
@@ -1040,7 +1051,7 @@ def create_crusader_black_knight(x: int, y: int, team: TeamType) -> int:
                             damage=gc.CRUSADER_BLACK_KNIGHT_ATTACK_DAMAGE,
                             recipient=Recipient.TARGET,
                             on_kill_effects=[
-                                CreatesAoE(
+                                CreatesVisualAoE(
                                     effects=[
                                         AppliesStatusEffect(
                                             status_effect=Fleeing(
@@ -1158,21 +1169,27 @@ def create_crusader_catapult(x: int, y: int, team: TeamType) -> int:
                         1: [
                             CreatesLobbed(
                                 effects=[
-                                    CreatesAoE(
+                                    CreatesCircleAoE(
                                         effects=[
                                             Damages(damage=gc.CRUSADER_CATAPULT_DAMAGE, recipient=Recipient.TARGET)
                                         ],
-                                        visual=Visual.CrusaderCatapultBallExplosion,
-                                        duration=gc.CRUSADER_CATAPULT_AOE_DURATION,
-                                        scale=gc.TINY_RPG_SCALE,
+                                        radius=25.0,
                                         unit_condition=All([Alive(), Grounded()]),
                                         location=Recipient.PARENT,
                                     ),
-                                    CreatePermanentVisual(
+                                    CreatesVisual(
+                                        recipient=Recipient.PARENT,
+                                        visual=Visual.CrusaderCatapultBallExplosion,
+                                        animation_duration=gc.CRUSADER_CATAPULT_AOE_DURATION,
+                                        scale=gc.TINY_RPG_SCALE,
+                                        duration=gc.CRUSADER_CATAPULT_AOE_DURATION,
+                                        layer=2,
+                                    ),
+                                    CreatesVisual(
                                         recipient=Recipient.PARENT,
                                         visual=Visual.CrusaderCatapultBallRemains,
                                         scale=gc.TINY_RPG_SCALE,
-                                        layer=-1,
+                                        layer=0,
                                         animation_duration=1,
                                     ),
                                     PlaySound(SoundEffect(filename="boulder_impact.wav", volume=0.60)),
@@ -1750,7 +1767,7 @@ def create_crusader_gold_knight(x: int, y: int, team: TeamType) -> int:
                     persistent_conditions=[],
                     effects={
                         0: [
-                            CreatesAoE(
+                            CreatesVisualAoE(
                                 effects=[
                                     Damages(damage=gc.CRUSADER_GOLD_KNIGHT_ATTACK_DAMAGE, recipient=Recipient.TARGET),
                                     Heals(amount=gc.CRUSADER_GOLD_KNIGHT_ATTACK_HEAL, recipient=Recipient.OWNER)
@@ -2342,7 +2359,7 @@ def create_crusader_red_knight(x: int, y: int, team: TeamType) -> int:
                     ],
                     persistent_conditions=[],
                     effects={7: [
-                        CreatesAoE(
+                        CreatesVisualAoE(
                             effects=[
                                 Damages(damage=gc.CRUSADER_RED_KNIGHT_SKILL_DAMAGE, recipient=Recipient.TARGET),
                                 AppliesStatusEffect(

src/events.py
@@ -34,15 +34,6 @@ class AbilityTriggeredEvent:
     index: int
 
 @dataclass
-class AoEHitEvent:
-    """Event triggered when an AoE hits a unit.
-    
-    Note that this doesn't mean the AoE will actually affect the unit.
-    """
-    entity: int
-    target: int
-
-@dataclass
 class AuraHitEvent:
     """Event triggered when an aura hits a unit.
     
@@ -62,6 +53,15 @@ class ChangeMusicVolumeEvent:
     """Event triggered to change the music volume."""
     volume: float
     """The new volume to set."""
+
+@dataclass
+class CircleAoEHitEvent:
+    """Event triggered when a CircleAoE hits a unit.
+    
+    Note that this doesn't mean the AoE will actually affect the unit.
+    """
+    entity: int
+    target: int
 
 @dataclass
 class DeathEvent:
@@ -129,15 +129,24 @@ class StateChangedEvent:
 class StopAllSoundsEvent:
     """Event triggered to stop all sounds."""
 
+@dataclass
+class VisualAoEHitEvent:
+    """Event triggered when a VisualAoE hits a unit.
+    
+    Note that this doesn't mean the AoE will actually affect the unit.
+    """
+    entity: int
+    target: int
+
 # Define signal names for each event
 ABILITY_ACTIVATED = 'ability_activated'
 ABILITY_COMPLETED = 'ability_completed'
 ABILITY_INTERRUPTED = 'ability_interrupted'
 ABILITY_TRIGGERED = 'ability_triggered'
-AOE_HIT = 'aoe_hit'
 AURA_HIT = 'aura_hit'
 CHANGE_MUSIC = 'change_music'
 CHANGE_MUSIC_VOLUME = 'change_music_volume'
+CIRCLE_AOE_HIT = 'circle_aoe_hit'
 DEATH = 'death'
 DESTINATION_TARGET_ACQUIRED = 'destination_target_acquired'
 DESTINATION_TARGET_LOST = 'destination_target_lost'
@@ -150,6 +159,7 @@ PLAY_VOICE = 'play_voice'
 PROJECTILE_HIT = 'projectile_hit'
 STATE_CHANGED = 'state_changed'
 STOP_ALL_SOUNDS = 'stop_all_sounds'
+VISUAL_AOE_HIT = 'visual_aoe_hit'
 
 def emit_event(event_type, **kwargs):
     """Emit an event using pydispatch."""

src/handlers/combat_handler.py
@@ -6,17 +6,16 @@ handling attack/skill/AoE/etc logic and applying damage/healing from attacks/ski
 
 import esper
 from components.ability import Abilities
-from components.aoe import AoE
+from components.aoe import VisualAoE, CircleAoE
 from components.aura import Aura
 from components.instant_ability import InstantAbilities
 from components.lobbed import Lobbed
 from components.projectile import Projectile
 from events import (
     LOBBED_ARRIVED, AbilityActivatedEvent, ABILITY_ACTIVATED,
-    AoEHitEvent, AOE_HIT,
     AuraHitEvent, AURA_HIT, LobbedArrivedEvent,
     ProjectileHitEvent, PROJECTILE_HIT,
-    InstantAbilityTriggeredEvent, INSTANT_ABILITY_TRIGGERED,
+    InstantAbilityTriggeredEvent, INSTANT_ABILITY_TRIGGERED, VisualAoEHitEvent, CircleAoEHitEvent, VISUAL_AOE_HIT, CIRCLE_AOE_HIT
 )
 from pydispatch import dispatcher
 
@@ -27,7 +26,8 @@ class CombatHandler:
         dispatcher.connect(self.handle_ability_activated, signal=ABILITY_ACTIVATED)
         dispatcher.connect(self.handle_instant_ability_triggered, signal=INSTANT_ABILITY_TRIGGERED)
         dispatcher.connect(self.handle_projectile_hit, signal=PROJECTILE_HIT)
-        dispatcher.connect(self.handle_aoe_hit, signal=AOE_HIT)
+        dispatcher.connect(self.handle_visual_aoe_hit, signal=VISUAL_AOE_HIT)
+        dispatcher.connect(self.handle_circle_aoe_hit, signal=CIRCLE_AOE_HIT)
         dispatcher.connect(self.handle_aura_hit, signal=AURA_HIT)
         dispatcher.connect(self.handle_lobbed_arrived, signal=LOBBED_ARRIVED)
 
@@ -63,27 +63,32 @@ class CombatHandler:
                 target=target_ent
             )
 
-    def handle_aoe_hit(self, event: AoEHitEvent):
+    def handle_visual_aoe_hit(self, event: VisualAoEHitEvent):
         aoe_ent = event.entity
         target_ent = event.target
-        aoe = esper.component_for_entity(aoe_ent, AoE)
-        if target_ent in aoe.hit_entities:
-            return
-        
-        if aoe.unit_condition.check(target_ent):
-            aoe.hit_entities.append(target_ent)
-            for effect in aoe.effects:
-                effect.apply(
-                    owner=aoe.owner,
-                    parent=aoe_ent,
-                    target=target_ent
-                )
+        aoe = esper.component_for_entity(aoe_ent, VisualAoE)
+        for effect in aoe.effects:
+            effect.apply(
+                owner=aoe.owner,
+                parent=aoe_ent,
+                target=target_ent
+            )
+    
+    def handle_circle_aoe_hit(self, event: CircleAoEHitEvent):
+        aoe_ent = event.entity
+        target_ent = event.target
+        aoe = esper.component_for_entity(aoe_ent, CircleAoE)
+        for effect in aoe.effects:
+            effect.apply(
+                owner=aoe.owner,
+                parent=aoe_ent,
+                target=target_ent
+            )
 
     def handle_aura_hit(self, event: AuraHitEvent):
         aura_ent = event.entity
         target_ent = event.target
         aura = esper.component_for_entity(aura_ent, Aura)
-        
         if aura.unit_condition.check(target_ent):
             for effect in aura.effects:
                 effect.apply(
@@ -101,4 +106,3 @@ class CombatHandler:
                 parent=lobbed_ent,
                 target=None,
             )
-

src/visuals.py
@@ -55,7 +55,7 @@ def create_visual_spritesheet(
         duration: Optional[float] = None,
         scale: Optional[float] = None,
         frames: Optional[Tuple[int, int]] = None,
-        layer: int = 0
+        layer: int = 1
 ) -> SpriteSheet:
     """Get the sprite sheet for a visual."""
     if visual == Visual.Arrow:

commit dcc12ec1c7a8492dde70c5dc448f5929b9014f59

Fix collision bug introduced in 64b92c3161a2f1915fbf1b195e86357993657532

src/processors/collision_processor.py
@@ -54,7 +54,7 @@ class CollisionProcessor(esper.Processor):
             circle_aoe_ents.append(ent)
         
         # For all sprites, update their collision masks
-        for sprite in [*team1_projectiles, *team2_projectiles, *team1_units, *team2_units]:
+        for sprite in [*team1_projectiles, *team2_projectiles, *team1_units, *team2_units, *visual_aoe_sprites]:
             sprite.mask = pygame.mask.from_surface(sprite.image, threshold=10)
 
         # Handle collisions between team1 projectiles and team2 units
@@ -177,6 +177,8 @@ class CollisionProcessor(esper.Processor):
             if u_ent in aoe.hit_entities:
                 continue
             if not aoe.unit_condition.check(u_ent):
+                continue
+            if not self.check_hitbox_collision(aoe_sprite, u_sprite, sprite_to_ent):
                 continue
             aoe.hit_entities.append(u_ent)
             emit_event(VISUAL_AOE_HIT, event=VisualAoEHitEvent(entity=aoe_ent, target=u_ent))