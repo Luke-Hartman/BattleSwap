6406f24d70a9206d7de72ecacd1cf33ebf3aada0
luke <luke.hartman@tmlt.io>
2025-08-02 10:30:21 -0700

    Upgrade tutorial panel enter.

--- src/ui_components/upgrade_tutorial_panel.py
+++ src/ui_components/upgrade_tutorial_panel.py
@@ -1,9 +1,15 @@
-"""UI component for displaying the upgrade tutorial when users first unlock upgrade points."""
+"""
+UI component for displaying the upgrade tutorial when users first unlock upgrade points.
+
+This panel appears when the player first gains the ability to upgrade units. It explains the upgrade system and can be dismissed by clicking the button or pressing Enter.
+"""
 
 import pygame
 import pygame_gui
 from pygame_gui.elements import UIPanel, UILabel, UIButton
 from screen_dimensions import get_width, get_height
+from keyboard_shortcuts import format_button_text, KeyboardShortcuts
+from events import emit_event, PLAY_SOUND, PlaySoundEvent
 
 
 class UpgradeTutorialPanel(UIPanel):
@@ -50,23 +56,26 @@ class UpgradeTutorialPanel(UIPanel):
         
         self.ok_button = UIButton(
             relative_rect=pygame.Rect((button_x, button_y), (button_width, button_height)),
-            text="Got it!",
+            text=format_button_text("Got it!", KeyboardShortcuts.ENTER),
             manager=manager,
             container=self
         )
     
     def handle_event(self, event: pygame.event.Event) -> bool:
-        """Handle button click events.
-        
+        """Handle button click events and Enter key.
+
         Args:
             event: The pygame event to handle.
-            
         Returns:
             True if the event was handled, False otherwise.
         """
         if (event.type == pygame.USEREVENT and 
             event.user_type == pygame_gui.UI_BUTTON_PRESSED and 
             event.ui_element == self.ok_button):
+            self.kill()
+            return True
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_RETURN:
+            emit_event(PLAY_SOUND, event=PlaySoundEvent(filename="ui_click.wav", volume=0.5))
             self.kill()
             return True
         return False 
